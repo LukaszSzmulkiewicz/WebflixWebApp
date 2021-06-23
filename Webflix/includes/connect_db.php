c36dee3eeeb4df5b02240267499d7742a57972e6
Lukasz Szmulkiewicz <55105517+LukaszSzmulkiewicz@users.noreply.github.com>
2021-06-23 15:19:32 +0100

    Update connect_db.php

--- Webflix/includes/connect_db.php
+++ Webflix/includes/connect_db.php
@@ -2,7 +2,7 @@
 
 /* Connect  on 'localhost' for database 'site_db'*/
 
-$link = mysqli_connect('localhost','HNDSOFTS2A37','tqCgDdP7yZ','HNDSOFTS2A37');
+$link = mysqli_connect('localhost','******','********','HNDSOFTS2A37');
 # If  username,password and database are correct 'connecton ok' will be dispalyed on screen.
 if (!$link) { 
 # Otherwise fail gracefully and explain the error. 
